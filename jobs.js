91c163b7d511fd011fe19b1bc41a1d5483d40a80
calmdownkarm <karmanya.aggarwal.14cse@bml.edu.in>
2016-11-27 12:44:57 +0530

    updated schema

--- jobs.js
+++ jobs.js
@@ -2,10 +2,30 @@ var mongoose = require('mongoose');
 var Category = require('./category');
 
 var jobSchema = {
-  title: { type: String, required: true },
-  location:{type: String, required: true},
+  title: { 
+  	type: String, 
+  	required: true 
+  },
+  location: {
+  	type: String, 
+  	required: true
+  },
+  description:{
+  	type: String,
+  	required: true
+  },
+  contactemail:{
+  	type: String,
+  	required: true,
+  	match: /.+@.+\..+/,
+    lowercase: true
+
+  }
   pay: {
-    amount: { type: Number, required: true },
+    amount: { 
+    	type: Number, 
+    	required: true
+    },
     currency: {
       type: String,
       enum: ['USD', 'INR'],
@@ -13,7 +33,7 @@ var jobSchema = {
     }
   },
   company:{type: String, required: true},
-  
+
   category: Category.categorySchema
 };
 
